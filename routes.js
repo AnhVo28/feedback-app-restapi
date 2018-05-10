b52b86ec2837cb3354d478e5dea1b037c14f172f
Anh Hoang Vo <vohoanganhkhtn@gmail.com>
2018-05-10 16:11:40 +0300

    Building API Route

--- routes.js
+++ routes.js
@@ -1,96 +1,67 @@
-'use strict';
+"use strict";
 
 var express = require("express");
 var router = express.Router();
 
 // GET /questions
-// Route for questions collection
-router.get("/", function(req, res){
-	res.json({response: "You sent me a GET request"});
+// Route for questions collections
+router.get("/", (req, res, next) => {
+  res.json({ response: "You sent me the GET request" });
 });
 
 // POST /questions
-// Route for creating questions
-router.post("/", function(req, res){
-	res.json({
-		response: "You sent me a POST request",
-		body: req.body
-	});
+// Route for creating the questions
+router.post("/", (req, res, next) => {
+  res.json({
+    response: "You sent the POST request",
+    body: req.body
+  });
 });
-
-// GET /questions/:id
+// GET /questions/:qID
 // Route for specific questions
-router.get("/:qID", function(req, res){
-	res.json({
-		response: "You sent me a GET request for ID " + req.params.qID
-	});
+router.get("/:qID", (req, res, next) => {
+  res.json({ response: "You sent me the GET request for ID " + req.params.id });
+});
+// POST /questions/:qID/answers
+// Route for specific questions
+router.post("/:qID/answers", (req, res, next) => {
+  res.json({
+    response: "You sent me the POST req to /answer",
+    body: req.body,
+    questionId: req.params.qID
+  });
 });
 
-// POST /questions/:id/answers
-// Route for creating an answer
-router.post("/:qID/answers", function(req, res){
-	res.json({
-		response: "You sent me a POST request to /answers",
-		questionId: req.params.qID,
-		body: req.body
-	});
-});
-
-// PUT /questions/:qID/answers/:aID
+// PUT /questions/:qID/answers/
 // Edit a specific answer
-router.put("/:qID/answers/:aID", function(req, res){
-	res.json({
-		response: "You sent me a PUT request to /answers",
-		questionId: req.params.qID,
-		answerId: req.params.aID,
-		body: req.body
-	});
+router.put("/:qID/answers/:aID", (req, res) => {
+  res.json({
+    response: "You sent me a PUT request to /answers",
+    questionId: req.params.qID,
+    answerId: req.params.aID,
+    body: req.body
+  });
 });
-
-// DELETE /questions/:qID/answers/:aID
+// DELETE /questions/:qID/answers/
 // Delete a specific answer
-router.delete("/:qID/answers/:aID", function(req, res){
-	res.json({
-		response: "You sent me a DELETE request to /answers",
-		questionId: req.params.qID,
-		answerId: req.params.aID
-	});
+router.delete("/:qID/answers/:aID", (req, res) => {
+  res.json({
+    response: "You sent me a DELETE request to /answers",
+    questionId: req.params.qID,
+    answerId: req.params.aID
+  });
 });
 
-// POST /questions/:qID/answers/:aID/vote-up
-// POST /questions/:qID/answers/:aID/vote-down
-// Vote on a specific answer
-router.post("/:qID/answers/:aID/vote-:dir", function(req, res, next){
-		if(req.params.dir.search(/^(up|down)$/) === -1) {
-			var err = new Error("Not Found");
-			err.status = 404;
-			next(err);
-		} else {
-			next();
-		}
-	}, function(req, res){
-	res.json({
-		response: "You sent me a POST request to /vote-" + req.params.dir,
-		questionId: req.params.qID,
-		answerId: req.params.aID,
-		vote: req.params.dir
-	});
+// POST /questions/:qID/answers/vote-up
+// POST /questions/:qID/answers/vote-down
+// Vote a specific answer
+router.post("/:qID/answers/:aID/vote-:dir", (req, res) => {
+  res.json({
+    response: "You sent me a POST request to /vote-" + req.params.dir,
+    questionId: req.params.qID,
+    answerId: req.params.aID,
+    vote: req.params.dir
+  });
 });
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
